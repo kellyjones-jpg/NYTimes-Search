63944cc4728dfe873669324e8a18ca62fbcee8ac
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2019-10-12 13:46:22 -0600

    Projects file uploads

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#run-search").on("click", function(e) {
+    e.preventDefault();
+    var searchTerm = document.getElementById("search-term").value;
+    var numSelector = document.getElementById("num-records-select").value;
+    var startYear = document.getElementById("start-year").value;
+    var endYear = document.getElementById("end-year").value;
+});
+
+
